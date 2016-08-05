da122893321bcf9f81a6f6757c071aba24644b7f
Brian Clifton <brian@clifton.me>
2016-08-05 15:29:30 -0700

    Tried a few things based on this post: https://msdn.microsoft.com/en-us/library/bb688195(VS.85).aspx

--- BrowserWindowStyles/main.cpp
+++ BrowserWindowStyles/main.cpp
@@ -1,11 +1,16 @@
 #include <windows.h>
 #include <string>
+#include <Dwmapi.h>
 
 // Demo to test win32 styles in preperation for updating Brave look/feel on Windows
-// Here's the refresher I used:
+// Helpful links:
 // https://msdn.microsoft.com/en-us/library/bb384843.aspx
+// https://msdn.microsoft.com/en-us/library/bb688195(VS.85).aspx
+// http://www.winprog.org/tutorial/menus.html
 #define DEMO_WINDOW_CLASS_NAME L"BrowserWindowStyles"
 #define DEMO_WINDOW_TITLE L"Window Title Goes Here"
+#define ID_FILE_QUIT 11
+#define ID_EDIT_TOGGLE 100
 
 WORD initializeWindowClass(WNDPROC eventHandler, HINSTANCE hInstance, std::wstring className) {
   WNDCLASSEX windowClass;
@@ -20,16 +25,44 @@ WORD initializeWindowClass(WNDPROC eventHandler, HINSTANCE hInstance, std::wstri
   windowClass.hInstance = hInstance;
   windowClass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
   windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-  windowClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-  windowClass.lpszMenuName = NULL; //TODO: needs a menu!
+  windowClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
   windowClass.lpszClassName = className.c_str();
   windowClass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
   
   return RegisterClassEx(&windowClass);
 }
 
+DWMNCRENDERINGPOLICY nonClientRenderingPolicy;
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
   switch (uMsg) {
+    case WM_ACTIVATE: {
+      MARGINS margins = { -1 };
+      margins.cxLeftWidth = 0;
+      margins.cxRightWidth = 0;
+      margins.cyBottomHeight = 20;
+      margins.cyTopHeight = 0;
+
+      DwmExtendFrameIntoClientArea(hWnd, &margins);
+
+      nonClientRenderingPolicy = DWMNCRP_DISABLED;
+      DwmSetWindowAttribute(hWnd, DWMWA_NCRENDERING_POLICY, &nonClientRenderingPolicy, sizeof(nonClientRenderingPolicy));
+      return NULL;
+    }
+
+    case WM_COMMAND:
+      switch (LOWORD(wParam)) {
+        case ID_FILE_QUIT:
+          PostQuitMessage(0);
+          return NULL;
+
+        case ID_EDIT_TOGGLE:
+          nonClientRenderingPolicy = nonClientRenderingPolicy == DWMNCRP_DISABLED ? DWMNCRP_ENABLED : DWMNCRP_DISABLED;
+          DwmSetWindowAttribute(hWnd, DWMWA_NCRENDERING_POLICY, &nonClientRenderingPolicy, sizeof(nonClientRenderingPolicy));
+          return NULL;
+      }
+      break;
+
     case WM_CLOSE:
     case WM_DESTROY:
     case WM_QUIT:
@@ -40,20 +73,62 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
   return DefWindowProc(hWnd, uMsg, wParam, lParam);
 }
 
+void AppendFileMenu(HMENU parentMenu) {
+  HMENU fileMenu = CreateMenu();
+
+  AppendMenu(fileMenu, MF_STRING, NULL, L"New Tab");
+  AppendMenu(fileMenu, MF_STRING, NULL, L"New Private Tab");
+  AppendMenu(fileMenu, MF_STRING, NULL, L"New Session Tab");
+  AppendMenu(fileMenu, MF_STRING, NULL, L"New Window");
+  AppendMenu(fileMenu, MF_SEPARATOR, NULL, NULL);
+  AppendMenu(fileMenu, MF_STRING, NULL, L"Open File...");
+  AppendMenu(fileMenu, MF_STRING, NULL, L"Open Location...");
+  AppendMenu(fileMenu, MF_SEPARATOR, NULL, NULL);
+  AppendMenu(fileMenu, MF_STRING, NULL, L"Close");
+  AppendMenu(fileMenu, MF_STRING, NULL, L"Close Window");
+  AppendMenu(fileMenu, MF_SEPARATOR, NULL, NULL);
+  AppendMenu(fileMenu, MF_STRING, NULL, L"Save Page as...");
+  AppendMenu(fileMenu, MF_SEPARATOR, NULL, NULL);
+  AppendMenu(fileMenu, MF_STRING, NULL, L"Print...");
+  AppendMenu(fileMenu, MF_SEPARATOR, NULL, NULL);
+  AppendMenu(fileMenu, MF_STRING, ID_FILE_QUIT, L"Quit Brave");
+
+  AppendMenu(parentMenu, MF_STRING | MF_POPUP, (UINT)fileMenu, L"File");
+}
+
+void AppendEditMenu(HMENU parentMenu) {
+  HMENU editMenu = CreateMenu();
+
+  AppendMenu(editMenu, MF_STRING, ID_EDIT_TOGGLE, L"BSC-toggle");
+
+  AppendMenu(parentMenu, MF_STRING | MF_POPUP, (UINT)editMenu, L"Edit");
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
   WORD result = initializeWindowClass(WndProc, hInstance, DEMO_WINDOW_CLASS_NAME);
+
+  HMENU menu = CreateMenu();
+  
+  AppendFileMenu(menu);
+  AppendEditMenu(menu);
+  AppendMenu(menu, NULL, NULL, L"View");
+  AppendMenu(menu, NULL, NULL, L"History");
+  AppendMenu(menu, NULL, NULL, L"Bookmarks");
+  AppendMenu(menu, NULL, NULL, L"Bravery");
+  AppendMenu(menu, NULL, NULL, L"Window");
+  AppendMenu(menu, NULL, NULL, L"Help");
 
   HWND hWnd = CreateWindowEx(
     WS_EX_LEFT,
     DEMO_WINDOW_CLASS_NAME,
     DEMO_WINDOW_TITLE,
-    (WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU),
+    WS_POPUPWINDOW | WS_CAPTION | WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_THICKFRAME,
     CW_USEDEFAULT,
     CW_USEDEFAULT,
     640,
     480,
     HWND_DESKTOP,
-    0, //TODO: needs a menu!
+    menu,
     hInstance, 0);
 
   ShowWindow(hWnd, nCmdShow);
