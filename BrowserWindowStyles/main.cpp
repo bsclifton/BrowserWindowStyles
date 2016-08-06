b6649ab35f0a4a1ce7655f1b8ac2bbc8d8250f9c
Brian Clifton <brian@clifton.me>
2016-08-05 17:19:16 -0700

    latest

--- BrowserWindowStyles/main.cpp
+++ BrowserWindowStyles/main.cpp
@@ -1,6 +1,9 @@
 #include <windows.h>
 #include <string>
 #include <Dwmapi.h>
+#include <Uxtheme.h>
+#include <vsstyle.h>
+#include <vssym32.h>
 
 // Demo to test win32 styles in preperation for updating Brave look/feel on Windows
 // Helpful links:
@@ -32,21 +35,126 @@ WORD initializeWindowClass(WNDPROC eventHandler, HINSTANCE hInstance, std::wstri
   return RegisterClassEx(&windowClass);
 }
 
-DWMNCRENDERINGPOLICY nonClientRenderingPolicy;
+//DWMNCRENDERINGPOLICY nonClientRenderingPolicy;
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
   switch (uMsg) {
-    case WM_ACTIVATE: {
-      MARGINS margins = { -1 };
-      margins.cxLeftWidth = 0;
-      margins.cxRightWidth = 0;
-      margins.cyBottomHeight = 20;
-      margins.cyTopHeight = 0;
+    //case WM_ACTIVATE: {
+    //  MARGINS margins;
+    //  margins.cxLeftWidth = 0;
+    //  margins.cxRightWidth = 0;
+    //  margins.cyBottomHeight = 0;
+    //  margins.cyTopHeight = 20;
 
-      DwmExtendFrameIntoClientArea(hWnd, &margins);
+    //  //DwmExtendFrameIntoClientArea(hWnd, &margins);
 
-      nonClientRenderingPolicy = DWMNCRP_DISABLED;
-      DwmSetWindowAttribute(hWnd, DWMWA_NCRENDERING_POLICY, &nonClientRenderingPolicy, sizeof(nonClientRenderingPolicy));
+    //  //nonClientRenderingPolicy = DWMNCRP_DISABLED;
+    //  //DwmSetWindowAttribute(hWnd, DWMWA_NCRENDERING_POLICY, &nonClientRenderingPolicy, sizeof(nonClientRenderingPolicy));
+    //  return NULL;
+    //}
+
+    case WM_PAINT: {
+      PAINTSTRUCT ps;
+      HDC hDC = BeginPaint(hWnd, &ps);
+
+      RECT rectClient;
+      GetClientRect(hWnd, &rectClient);
+      FillRect(hDC, &rectClient, (HBRUSH)GetStockObject(BLACK_BRUSH));
+
+      //https://msdn.microsoft.com/en-us/library/windows/desktop/bb773210(v=vs.85).aspx
+
+      RECT rectMinimizeButton;
+      rectMinimizeButton.left = rectClient.right - 60;
+      rectMinimizeButton.right = rectClient.right - 40;
+      rectMinimizeButton.top = 0;
+      rectMinimizeButton.bottom = 20;
+      DrawFrameControl(hDC, &rectMinimizeButton, DFC_CAPTION, DFCS_CAPTIONMIN);
+
+      RECT rectMaximizeButton;
+      rectMaximizeButton.left = rectClient.right - 40;
+      rectMaximizeButton.right = rectClient.right - 20;
+      rectMaximizeButton.top = 0;
+      rectMaximizeButton.bottom = 20;
+      DrawFrameControl(hDC, &rectMaximizeButton, DFC_CAPTION, DFCS_CAPTIONMAX);
+
+      RECT rectCloseButton;
+      rectCloseButton.left = rectClient.right - 20;
+      rectCloseButton.right = rectClient.right;
+      rectCloseButton.top = 0;
+      rectCloseButton.bottom = 20;
+      DrawFrameControl(hDC, &rectCloseButton, DFC_CAPTION, DFCS_CAPTIONCLOSE);
+
+      // https://msdn.microsoft.com/en-us/library/aa969233.aspx
+      TITLEBARINFOEX info = { sizeof(info) };
+
+      #define BSC_TITLEBAR 0
+      #define BSC_RESERVED 1
+      #define BSC_MINIMIZE 2
+      #define BSC_MAXMIZE 3
+      #define BSC_HELP 4
+      #define BSC_CLOSE 5
+      //https://blogs.msdn.microsoft.com/oldnewthing/20140505-00/?p=1083
+      if (!SendMessage(hWnd, WM_GETTITLEBARINFOEX, 0, (LPARAM)&info)) {
+        return FALSE;
+      }
+      int closeWidth = info.rgrect[BSC_CLOSE].right - info.rgrect[BSC_CLOSE].left;
+      int closeHeight = info.rgrect[BSC_CLOSE].bottom - info.rgrect[BSC_CLOSE].top;
+      int maximizeWidth = info.rgrect[BSC_MAXMIZE].right - info.rgrect[BSC_MAXMIZE].left;
+      int maximizeHeight = info.rgrect[BSC_MAXMIZE].bottom - info.rgrect[BSC_MAXMIZE].top;
+      int minimizeWidth = info.rgrect[BSC_MINIMIZE].right - info.rgrect[BSC_MINIMIZE].left;
+      int minimizeHeight = info.rgrect[BSC_MINIMIZE].bottom - info.rgrect[BSC_MINIMIZE].top;
+
+      /*
+      //another way to get size/bounds for the min/max/close buttons
+      if (false) {
+        //https://msdn.microsoft.com/en-us/library/windows/desktop/aa969515(v=vs.85).aspx
+        RECT bounds;
+        DwmGetWindowAttribute(hWnd, DWMWA_CAPTION_BUTTON_BOUNDS, &bounds, sizeof(RECT));
+        closeWidth = bounds.right - bounds.left;
+        closeHeight = bounds.bottom - bounds.top;
+      }*/
+
+      /*
+      //didn't get a chance to look at this all the way
+
+      HTHEME hTheme = OpenThemeData(hWnd, L"DWMWINDOW");
+      RECT rectMin;
+      GetThemeRect(hTheme, WP_MINCAPTION, MNCS_ACTIVE, TMT_ATLASRECT, &rectMin);
+      //GetThemeStream(...);
+      CloseThemeData(hTheme);
+      FillRect(hDC, &rectMin, (HBRUSH)GetStockObject(GRAY_BRUSH));
+      */
+
+
+      //http://stackoverflow.com/questions/34004819/windows-10-close-minimize-and-maximize-buttons
+      SetWindowTheme(hWnd, L"explorer", NULL);
+      HTHEME hTheme = OpenThemeData(hWnd, L"WINDOW");
+
+      RECT rectCloseButton2;
+      rectCloseButton2.left = rectClient.right - closeWidth;
+      rectCloseButton2.right = rectClient.right;
+      rectCloseButton2.top = 20;
+      rectCloseButton2.bottom = rectCloseButton2.top + closeHeight;
+      DrawThemeBackground(hTheme, hDC, WP_CLOSEBUTTON, CBS_NORMAL, &rectCloseButton2, NULL);
+
+      RECT rectMaximizeButton2;
+      rectMaximizeButton2.right = rectCloseButton2.left;
+      rectMaximizeButton2.left = rectMaximizeButton2.right - maximizeWidth;
+      rectMaximizeButton2.top = 20;
+      rectMaximizeButton2.bottom = rectMaximizeButton2.top + maximizeHeight;
+      DrawThemeBackground(hTheme, hDC, WP_MAXBUTTON, MAXBS_NORMAL, &rectMaximizeButton2, NULL);
+
+      RECT rectMinimizeButton2;
+      rectMinimizeButton2.right = rectMaximizeButton2.left;
+      rectMinimizeButton2.left = rectMinimizeButton2.right - minimizeWidth;
+      rectMinimizeButton2.top = 20;
+      rectMinimizeButton2.bottom = rectMinimizeButton2.top + minimizeHeight;
+      DrawThemeBackground(hTheme, hDC, WP_MINBUTTON, MINBS_NORMAL, &rectMinimizeButton2, NULL);
+
+      CloseThemeData(hTheme);
+
+
+      EndPaint(hWnd, &ps);
       return NULL;
     }
 
@@ -56,10 +164,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
           PostQuitMessage(0);
           return NULL;
 
-        case ID_EDIT_TOGGLE:
-          nonClientRenderingPolicy = nonClientRenderingPolicy == DWMNCRP_DISABLED ? DWMNCRP_ENABLED : DWMNCRP_DISABLED;
-          DwmSetWindowAttribute(hWnd, DWMWA_NCRENDERING_POLICY, &nonClientRenderingPolicy, sizeof(nonClientRenderingPolicy));
-          return NULL;
+        //case ID_EDIT_TOGGLE:
+          //nonClientRenderingPolicy = nonClientRenderingPolicy == DWMNCRP_DISABLED ? DWMNCRP_ENABLED : DWMNCRP_DISABLED;
+          //DwmSetWindowAttribute(hWnd, DWMWA_NCRENDERING_POLICY, &nonClientRenderingPolicy, sizeof(nonClientRenderingPolicy));
+          //return NULL;
       }
       break;
 
